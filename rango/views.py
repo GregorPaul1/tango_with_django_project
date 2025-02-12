8287efe477bafd7816882b8fec0d7f734e2de679
Gregor Paul <2895645p@student.gla.ac.uk>
2025-02-12 13:45:06 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 # Create your views here.
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Gregor Paul'}
+    return render(request, 'rango/about.html', context = context_dict)
